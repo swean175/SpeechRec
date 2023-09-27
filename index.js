88f727bf7028cfe167bdf423f2adfa6065232777
Damian <swean@vp.pl>
2023-09-27 19:38:14 +0200

    2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getDatabase, ref, push, get, remove } from 'firebase/database'
 
 import { Configuration, OpenAIApi } from 'openai'
-import { process } from './env'
+
 
 
 
