6ff16acf9c6bac04aa1ca83765b9e0a8bf52d4fa
Damian <swean@vp.pl>
2023-09-27 19:36:13 +0200

    1

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ import { Configuration, OpenAIApi } from 'openai'
 import { process } from './env'
 
 
+
+const userInput = document.getElementById('user-input')
 const talkBtn = document.getElementById('talk')
 const content = document.getElementById('content')
 //--------------------Speech
 const speechRec = window.SpeechRecognition || window.webkitSpeechRecognition
 const recognition = new speechRec()
 recognition.lang = 'pl'
+
+
 
 //-------------------------------
 const configuration = new Configuration({
@@ -33,12 +37,12 @@ const chatbotConversation = document.getElementById('chatbot-conversation')
 
 const instructionObj = {
     role: 'system',
-    content: 'You are a helpful, flirty, funny, teasy assistant, your name is Ana.'
+    content: 'You are a helpful, flirty, funny, teasy assistant, your name is Ana my name is Damian and i am amzing'
 }
 
 document.addEventListener('submit', (e) => {
     e.preventDefault()
-    const userInput = document.getElementById('user-input')
+  
     push(conversationInDb, {
         role: 'user',
         content: userInput.value
@@ -52,7 +56,9 @@ document.addEventListener('submit', (e) => {
     chatbotConversation.scrollTop = chatbotConversation.scrollHeight
 })
 
-function fetchReply() {
+
+
+async function fetchReply() {
     get(conversationInDb).then(async (snapshot) => {
         if (snapshot.exists()) {
             const conversationArr = Object.values(snapshot.val())
@@ -64,9 +70,9 @@ function fetchReply() {
                 frequency_penalty: 0.3
             })
             push(conversationInDb, response.data.choices[0].message)
-            renderTypewriterText()
-            readOutLoud(response.data.choices[0].message.content)
-        
+            renderTypewriterText(response.data.choices[0].message.content)
+            elevenSpeak(response.data.choices[0].message.content+".....")
+            
         }
         else {
            alert('No data available')
@@ -74,6 +80,8 @@ function fetchReply() {
 
     })
 }
+
+
 
 function renderTypewriterText(text) {
     const newSpeechBubble = document.createElement('div')
@@ -112,15 +120,126 @@ function renderConversationFromDb(){
         }
     })
 }
-//-------------------new Speech Api--------------
 
+//------------------------Eleven----------
 
+function elevenSpeak(message){
+
+    let audioDataArray = []
+const AudioContext = window.AudioContext || window.webkitAudioContext
+const audioContext = new AudioContext();
+let nextTime = 0; // This variable will keep track of the time the next chunk should start
+
+    const voiceId = "XrExE9yKIg1WjnnlVkGX"; // replace with your voice_id
+        const model = 'eleven_monolingual_v1';
+        const wsUrl = `wss://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream-input?model_id=${model}&optimize_streaming_latency=4&output_format=mp3_44100`;
+        const socket = new WebSocket(wsUrl);
+       
+    // 2. Initialize the connection by sending the BOS message
+    socket.onopen = function (event) {
+
+        const bosMessage = {
+            "text": " ",
+            "voice_settings": {
+                "stability": 0.5,
+                "similarity_boost": true
+            },
+            "xi_api_key": process.env.Eleven_API_KEY, // replace with your API key
+        };
+    
+        socket.send(JSON.stringify(bosMessage));
+    
+        // 3. Send the input text message ("Hello World")
+        
+       const textMessage = {
+            "text": ` ${message},,,,, `,
+            "try_trigger_generation": true,
+        };
+       socket.send(JSON.stringify(textMessage)) ;
+
+        // 4. Send the EOS message with an empty string
+        const eosMessage = {
+            "text": ""
+        };
+    
+        socket.send(JSON.stringify(eosMessage));
+    };
+    
+    // 5. Handle server responses
+    socket.onmessage = async function (event) {
+        const response = await JSON.parse(event.data);
+
+        console.log("Server response:", response);
+    
+        if (response.audio) {
+            // decode and handle the audio data (e.g., play it)
+      // convert base64 to Uint8Array
+    
+      const data = Uint8Array.from(atob(response.audio), c => c.charCodeAt(0));
+              
+
+        // decode the audio data asynchronously 
+        const buffer = await audioContext.decodeAudioData(data.buffer).then(decodedAudio => { audioDataArray.push(decodedAudio)});
+
+        
+let source
+        audioDataArray.forEach( decodedAudio => { 
+             source = audioContext.createBufferSource() 
+            source.buffer = decodedAudio
+             source.connect(audioContext.destination)
+            
+            })
+     
+        // Start the source to play the audio
+        if (nextTime == 0) {
+            nextTime = audioContext.currentTime;
+            }
+            for(let i = 0; i < audioDataArray.length; i++)
+           { 
+            source.start(nextTime)
+            nextTime += source.buffer.duration
+        }
+        
+   
+ //----------------------------------------------------------------------------
+      
+            console.log("Received audio chunk");
+        } else {
+            console.log("No audio data in the response");
+        }
+    
+        if (response.isFinal) {
+            // the generation is complete
+           
+        }
+  
+        if (response.normalizedAlignment) {
+            // use the alignment info if needed
+    
+        }
+
+    // Handle errors
+    socket.onerror = function (error) {
+        console.error(`WebSocket Error: ${error}`);
+    };
+    
+    // Handle socket closing
+    socket.onclose = function (event) {
+        if (event.wasClean) {
+            console.info(`Connection closed cleanly, code=${event.code}, reason=${event.reason}`);
+        } else {
+            console.warn('Connection died');
+        }
+    };
+
+    }
+
+}
 //---------------rest of speech------------------
 
 
 recognition.onstart = function(){
-   
-    
+
 }
 
 recognition.onresult = function(event){
@@ -135,17 +254,17 @@ talkBtn.addEventListener('click', () => {
     recognition.start()
 })
 
-function readOutLoud(message){
-    const speech = new SpeechSynthesisUtterance()
+// function readOutLoud(message){
+//     const speech = new SpeechSynthesisUtterance()
 
-    speech.text = message
-    speech.volume = 1
-    speech.rate = 1
-    speech.pitch = 1
+//     speech.text = message
+//     speech.volume = 1
+//     speech.rate = 1
+//     speech.pitch = 1
   
   
-    window.speechSynthesis.speak(speech)
-}
-
+//     window.speechSynthesis.speak(speech)
+// }
 renderConversationFromDb()
+
 
